379fcfe209f8212fc075d14d92a0b372e0927f2a
abed <abed.bilani@gmail.com>
2018-08-07 23:12:32 +0300

    first version of the meetup place

---
+++ ChameleonsSimulation/src/chameleonssimulation/Forest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chameleonssimulation;
+
+/**
+ *
+ * @author Abed Bilani
+ */
+public class Forest {
+    // place where the chameleon will meet 
+    
+    // when chameleon meet we need to check 
+    
+    // 
+}
