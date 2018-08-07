5edcf46efcf7de1cc13db2ac339708e1c762b0f1
abed <abed.bilani@gmail.com>
2018-08-07 23:11:18 +0300

    adding color object

---
+++ ChameleonsSimulation/src/chameleonssimulation/ChameleonColor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chameleonssimulation;
+
+/**
+ *
+ * @author Abed Bilani
+ */
+public class ChameleonColor {
+    // in this class will generate a color for each created chameleon
+    // depending on the 3 colors we have as default it will be 0 1 2 => blue yellow red
+    
+}
