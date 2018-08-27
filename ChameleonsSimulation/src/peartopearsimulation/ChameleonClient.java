8faa104753ca7cdda9d2f0c498875b2c1b5c4a11
abed bilani <abedelaziz.bilani@isae.edu.lb>
2018-08-27 20:58:58 +0300

    adding new solution

---
+++ ChameleonsSimulation/src/peartopearsimulation/ChameleonClient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peartopearsimulation;
+
+/**
+ * in this we will create a client class in which we will connect to main server 
+ * 
+ * @author abed.bilani
+ */
+public class ChameleonClient {
+    
+    // add buffers to read and write to the socket
+    
+    // main class in which we will launch a thread and connect to server socket
+    
+}
---
+++ ChameleonsSimulation/src/peartopearsimulation/ChameleonServer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peartopearsimulation;
+
+/**
+ * in this class we will add a server for sockets
+ * all chameleons will connect to this socket and will launch a new thread
+ * @author abed.bilani
+ */
+public class ChameleonServer {
+    
+    // add main 
+    // in main launch server socket 
+    // accept clients 
+    // and start a thread
+}
